c557d03378e04b901c31588862c94d4ee64b01cb
Jm San Diego <jmsandiego0710@gmail.com>
2021-09-30 23:15:57 +0800

    Added amplify and routing

---
+++ src/features/landing/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Landing() {
+  return (
+    <>
+      <h1>Landing Page</h1>
+      <p>Details</p>
+    </>
+  );
+}
---
+++ src/features/landing/Register.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Register() {
+  return (
+    <>
+      <h1>Register Page</h1>
+      <p>Register Page Details</p>
+    </>
+  );
+}
