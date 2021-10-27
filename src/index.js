a100c858706409424b40252f1443f0bd44245f03
jmsandiegoo <jmsandiego0710@gmail.com>
2021-10-27 15:46:49 +0800

    Reworked amplify with another account and database

--- src/index.js
+++ src/index.js
@@ -28,3 +28,62 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+/* Amplify Params - DO NOT EDIT
+	API_SIMPLETWITTERCLONE_GRAPHQLAPIENDPOINTOUTPUT
+	API_SIMPLETWITTERCLONE_GRAPHQLAPIIDOUTPUT
+	API_SIMPLETWITTERCLONE_GRAPHQLAPIKEYOUTPUT
+	AUTH_SIMPLETWITTERCLONED8CE95B6_USERPOOLID
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+/*
+const axios = require('axios');
+const gql = require('graphql-tag');
+const  graphql = require('graphql');
+const { print } = graphql;
+
+const createUser = gql`
+mutation createUser($input: CreateUserInput!) {
+    createUser(input: $input) {
+        username
+        name
+        email
+        birth_date
+    }
+}
+`
+
+exports.handler = (event, context, callback) => {
+    console.log(event);
+    const { username, email, name, birthdate } = event.request.userAttributes;
+
+    // Request graphql mutation
+    axios({
+        url: process.env.API_SIMPLETWITTERCLONE_GRAPHQLAPIENDPOINTOUTPUT,
+        method: 'post',
+        headers: {
+          'x-api-key': process.env.API_SIMPLETWITTERCLONE_GRAPHQLAPIKEYOUTPUT
+        },
+        data: {
+          query: print(createUser),
+          variables: {
+            input: {
+              username,
+              email,
+              name,
+              birthdate
+            }
+          }
+        }
+    })
+    .then(() => {
+        callback(null, event)
+    })
+    .catch((error) => {
+        callback(Error(error));
+    })
+}
+*/
